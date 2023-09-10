91834f6c817525a7f2b3ca91f5578d537c0f7736
[иHolovnya Stanislav <golovnastanislav@gmail.com>
2023-09-11 00:33:09 +0300

    hello Kiev

--- src/Main/Java/ua/hillel/Holovnya/lessons/lesson3/Main.java
+++ src/Main/Java/ua/hillel/Holovnya/lessons/lesson3/Main.java
@@ -3,7 +3,7 @@ package ua.hillel.Holovnya.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
-         System.out.println("Hello fromGitHub");
+        System.out.println("Hello fromGitHub");
         System.out.println("Hello FromKiev");
     }
 }
