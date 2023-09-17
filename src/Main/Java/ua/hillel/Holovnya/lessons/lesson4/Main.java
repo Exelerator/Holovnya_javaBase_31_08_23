1b5b5bf9449a5405aa5121a939f6a15b62aa2730
[иHolovnya Stanislav <golovnastanislav@gmail.com>
2023-09-18 02:27:35 +0300

    hello Kiev

---
+++ src/Main/Java/ua/hillel/Holovnya/lessons/lesson4/Main.java
@@ -0,0 +1,141 @@
+package ua.hillel.Holovnya.lessons.lesson4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        System.out.println(10-5);
+//        System.out.println(10+5);
+//        System.out.println(10*5);
+//        System.out.println(10/5);
+//        System.out.println();
+//
+//        int a=10;
+//        int b=5;
+//
+//        System.out.println(a-b);
+//        System.out.println(a+b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+//
+//        System.out.println(10/3);
+//        System.out.println(10.0/3);
+//        int a=10;
+//        double b=3;
+//        System.out.println(a/b);
+//
+//        int a=10;
+//        int b=3;
+//        System.out.println(a%b);
+
+//        int a=10;
+//        int b=5;
+//
+//        a=a+10;
+//        System.out.println(a);
+//
+//
+//        a +=10;
+//        System.out.println(a);
+//         int a=5;
+////         a++;
+//        System.out.println(a);
+//        a--;
+//        System.out.println(a);
+//        int a=5;
+//        int b=2;
+//        System.out.println(a++ + ++a -b--);
+//        System.out.println("value a="+a);
+//        System.out.println("value b="+b);
+
+
+//        int a=700;
+//        int b=2100;
+//        int c=3500;
+//
+//        int countMonthYears=10*12;
+//
+//
+//        int salary1=a*countMonthYears;
+//        int salary2=b*countMonthYears;
+//        int salary3=c*countMonthYears;
+//
+//        System.out.println(salary1);
+//        System.out.println(salary2);
+//        System.out.println(salary3);
+//
+//        double Tax= 0.95;
+//
+//
+//        int salvary1WithoutTax= (int)(salary1*Tax);
+//        int salvary2WithoutTax=(int)(salary2*Tax);
+//        int salvary3WithoutTax=(int)(salary3*Tax);
+//
+//        System.out.println(salvary1WithoutTax);
+//        System.out.println(salvary2WithoutTax);
+//        System.out.println(salvary3WithoutTax);
+//
+//        double sumSalary=salvary1WithoutTax+salvary2WithoutTax+salvary3WithoutTax;
+//        double avgSalary=sumSalary/3;
+//        System.out.println("sumSalary="+sumSalary);
+//        System.out.println("avgSalary="+avgSalary);
+//
+//        System.out.println(Math.PI);
+//
+//        System.out.println(Math.abs(10));;
+//        System.out.println(Math.abs(-10));
+
+//        System.out.println(Math.pow(2, 2));
+//        System.out.println(Math.pow(2, 3));
+//        System.out.println(Math.pow(2.2, 4.5));
+
+//        System.out.println(Math.sqrt(8));
+//        System.out.println(Math.sqrt(16));
+
+//        System.out.println(Math.round(10.4));
+//        System.out.println(Math.round(10.5));
+
+//        System.out.println(Math.min(-10, 10));
+//        System.out.println(Math.max(-10, 10));
+
+
+//        System.out.println(Math.random());//(0.0-1);
+
+//        System.out.println((int)(Math.random()*11));//(0-10)
+//        System.out.println((int)(Math.random()*21)+15);//(15-35)
+//
+//
+//        int min=15;
+//        int max=35;
+//        System.out.println((int)(Math.random()*(max-min+1)+min));
+
+//        int a=9;
+//        int b=6;
+//        int c=3;
+//
+//        int volume=a*b*c;
+//        System.out.println("parallelepiped volume="+volume);
+//
+//        int length=a+b+c;
+//        System.out.println("length="+length);
+
+
+        int fighterLi=13;
+        int archerLi=24;
+        int riderLi=46;
+
+        int forceLi=(archerLi+fighterLi+riderLi);
+        System.out.println("Li="+forceLi*860);
+
+
+        int fighterMin=9;
+        int archerMin=35;
+        int riderMin=12;
+        int forceMin=(fighterMin+archerMin+riderMin);
+        int allFocreMin=(int)((forceMin)*860*1.5);
+        System.out.println(("Min="+allFocreMin));
+
+
+    }
+
+}
