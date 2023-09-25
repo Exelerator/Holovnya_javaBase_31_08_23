51c36605cdbfc98c271454fa6ba3add9adba398a
[иHolovnya Stanislav <golovnastanislav@gmail.com>
2023-09-25 23:06:34 +0300

    Counter Strike

--- src/Main/Java/ua/hillel/Holovnya/lessons/lesson6/Japan.java
+++ src/Main/Java/ua/hillel/Holovnya/lessons/lesson6/Japan.java
@@ -3,19 +3,14 @@ package ua.hillel.Holovnya.lessons.lesson6;
 public class Japan {
     public static void main(String[] args) {
 
-
-        int counter=99;
-        for (int i = 0; i<9; i++) {
-            for (int j = 0; j < 9; j++) {
-                for (int k = 0; k < 9; k++) {
-                    if (i != 4 && j != 4 && k != 4)
-                        System.out.println(i + "" + j + "" + k);
-
-                }
+        int count = 0;
+        for (int i = 0; count<= 100; i++) {
+            if (i == 4 || i == 9 || i % 10 == 4 || i % 10 == 9 || i/10==4 || i/10==9|| (i-100)/10==4 || (i-100)/10==9) {
+                continue;
             }
+            System.out.println(i);
+            count++;
         }
-        counter++;
-        System.out.println("counter="+counter);
 
 
     }
