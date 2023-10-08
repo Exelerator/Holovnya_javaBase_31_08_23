39b84a2e14c775b86566c3e5d577efff21a9824c
[иHolovnya Stanislav <golovnastanislav@gmail.com>
2023-10-08 20:07:36 +0300

    HomeWork8.java

--- src/Main/Java/ua/hillel/Holovnya/lessons/lesson9/Matrix.java
+++ src/Main/Java/ua/hillel/Holovnya/lessons/lesson9/Matrix.java
@@ -5,9 +5,26 @@ import java.util.Scanner;
 public class Matrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        System.out.println("Please enter number");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+                if (number < 0) {
+                    System.out.println("Please enter number again");
+                } else {
+                    break;
+                }
+                scanner.nextLine();
+            } else {
+                System.out.println("Wrong data");
+                scanner.nextLine();
+            }
+        }
 
-        int N =(int) (Math.random() * 11);
-        int M = (int) (Math.random() * 11);
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
 
         int[][] array1 = new int[N][M];
         int[][] array2 = new int[M][N];
@@ -32,22 +49,6 @@ public class Matrix {
                 System.out.print(array1[i][j] + "\t");
             }
             System.out.println();
-        }
-
-        int number = 0;
-        System.out.println("Please enter number");
-        while(true) {
-        if (scanner.hasNextInt()) {
-            number = scanner.nextInt();
-            if (number < 0) {
-                System.out.println("Please enter number again");
-            } else {
-                break;
-            }
-            scanner.nextLine();
-        } else {
-            System.out.println("Wrong data");
-            scanner.nextLine();}
         }
 
     }
