12d59c1cf58d8a9915982d05722b526047ced080
[иHolovnya Stanislav <golovnastanislav@gmail.com>
2023-10-12 01:01:37 +0300

    Person.java

--- src/Main/Java/ua/hillel/Holovnya/lessons/lesson10/Person.java
+++ src/Main/Java/ua/hillel/Holovnya/lessons/lesson10/Person.java
@@ -7,6 +7,7 @@ public class Person {
         System.out.println(personInfo("Sherlock","Holmes","London","3712341234"));
     }
 
+
     static String personInfo(String firstName,String secondName,String city,String phoneNumber) {
         return "Зателефонувати громадянинові " + firstName + " " + secondName + " з міста " + city + " можна за номером " + phoneNumber + ".";
 
